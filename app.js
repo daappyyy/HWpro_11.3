e6c15f89d61c4799faa7f62b04525a9c87289a01
Sofie <solianyksofie@gmail.com>
2024-09-25 18:11:32 +0200

    Task 11.3

---
+++ app.js
@@ -0,0 +1,19 @@
+function getRandomNum(){
+    return Math.floor((Math.random() * 9) + 1);
+}
+const fileNumber = getRandomNum();
+const newImg = document.createElement('img');
+const cardImg = document.querySelector('.card-image');
+const refreshButton = document.querySelector('.refresh-btn')
+
+
+refreshButton.addEventListener('click', HandleUserClick);
+function HandleUserClick(){
+    const fileNumber = getRandomNum();
+    newImg.src = `images/${fileNumber}.jpg`;
+    newImg.width = 300;
+    newImg.height = 300;
+    newImg.alt = 'Zhorik';
+    cardImg.innerHTML = ''; 
+    cardImg.appendChild(newImg);
+}
